0eb6526a13563004ecf12cfb7b4b1f8d3436292d
Michael Cattell <mjclego@icloud.com>
2025-03-12 08:40:40 -0400

    edits

--- read_arrow.R
+++ read_arrow.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 
 read_arrow <- function(filename) {
+  date <- as.Date(substring(gsub(".txt", "", filename), 
+                            nchar(filename) - 11), "%Y%m%d")
+  train_no <- gsub("\\/.*", "", gsub("Data//StatusMap Delay Data//2024 Unzipped Data/", "", filename))
   data <- filename
   data_lines <- readLines(data)
   data_lines <- data_lines[-c(1)]
@@ -18,9 +21,47 @@ read_arrow <- function(filename) {
                  widths = widths,
                  header = FALSE,
                  skip = 9,
-                 col.names = col_names) %>%
-    select(StationCode, ScheduleDepartureDay, S)
+                 col.names = col_names) %>% 
+    mutate("star" = gsub(" ", "", star)) %>% 
+    filter(star == "*") %>%
+    select(-star)
   df <- df[-c(1),]
-  df
+  df <- df %>%
+    mutate(ScheduleDepartureTime = gsub(" ", "", ScheduleDepartureTime)) %>%
+    mutate(ScheduleDepartureTime = ifelse(nchar(ScheduleDepartureTime) <= 4, 
+                                          paste("0", ScheduleDepartureTime, "M", sep = ""),
+                                          paste(ScheduleDepartureTime, "M", sep = ""))) %>%
+    mutate(ActualDepartureTime = gsub(" ", "", ActualDepartureTime)) %>%
+    mutate(ActualDepartureTime = ifelse(nchar(ActualDepartureTime) <= 4, 
+                                        paste("0", ActualDepartureTime, "M", sep = ""),
+                                        paste(ActualDepartureTime, "M", sep = ""))) %>%
+    filter(!grepl("\\*", ScheduleDepartureTime) & ActualDepartureTime != "0M") %>%
+    mutate("ScheduleDepartureDay" = gsub(" ", "", ScheduleDepartureDay),
+           "adjusted_day" = as.numeric(ScheduleDepartureDay) - 1,
+           "SchDepartureDate" = date + adjusted_day)
+  
+  stringi::stri_sub(df$ScheduleDepartureTime, 3, 2) <- ":"
+  stringi::stri_sub(df$ActualDepartureTime, 3, 2) <- ":"
+  
+  df <- df %>%
+    mutate(Comments = ifelse(grepl("Arrived", Comments), gsub(".*\\|", "", Comments), Comments),
+           comments = gsub("[^0-9,-]", "", Comments)) %>%
+    select(-Comments) %>%
+    mutate(comments = as.numeric(comments))
+  
+  df$comments[is.na(df$comments)] <- 0
+  
+  df <- df %>%
+    mutate(hour_diff = ifelse(grepl(",", comments), as.numeric(gsub("\\,.*", "", comments)), 0),
+           min_diff = ifelse(grepl(",", comments), as.numeric(gsub(".*\\,", "", comments)), comments),
+           min_diff = ifelse(is.na(min_diff), 0, as.numeric(min_diff)),
+           sec_diff = (hour_diff * 60 * 60) + (min_diff * 60),
+           sec_diff = ifelse(late_check == "late", sec_diff * 1, sec_diff * -1),
+           sch_dep_dt = as.POSIXct(paste(SchDepartureDate, ScheduleDepartureTime, sep = " "), format = "%Y-%m-%d %I:%M%p"),
+           act_dep_dt = as.POSIXct(sch_dep_dt + sec_diff, format = "%Y-%m-%d %I:%M%p"))
+  
+  df %>% 
+    select(StationCode, sec_diff, sch_dep_dt, act_dep_dt) %>%
+    mutate(train_number = train_no)
 }
-read_arrow("test_file.txt")
+read_arrow("Data//StatusMap Delay Data//2024 Unzipped Data/665/665_20241102.txt")
---
+++ status_map_delay_data_load.R
@@ -0,0 +1,25 @@
+library(plyr)
+
+# zipped_folders <- list.files(path = "Data//StatusMap Delay Data//2024", pattern = "*.zip", full.names = TRUE)
+# 
+# ldply(.data = zipped_folders, .fun = unzip, exdir = "Data//StatusMap Delay Data//2024 Unzipped Data")
+# 
+# unlink("Data//StatusMap Delay Data//2024 Unzipped Data/logs", recursive = TRUE)
+
+delay_data_folders_2024 <- list.dirs("Data//StatusMap Delay Data//2024 Unzipped Data")
+
+read_arrow_wrapper <- function(filename) {
+  tryCatch(read_arrow(filename),
+           error = function(e) NULL)
+}
+
+delay_data <- NULL
+for (i in delay_data_folders_2024) {
+  files <- list.files(path = i, pattern = ".txt", full.names = TRUE)
+  files <- files[-1]
+  df <- ldply(.data = files, .fun = read_arrow_wrapper)
+  delay_data <- rbind(delay_data, df)
+  
+  print(paste("Completed", i, sep = " "))
+}
+
