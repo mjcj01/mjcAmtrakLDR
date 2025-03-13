21194c241da58c066c47a5edb8ca48c10a2aa107
Michael Cattell <mjclego@icloud.com>
2025-03-13 15:02:43 -0400

    edits

--- Scripts/late_classification.R
+++ Scripts/late_classification.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-delay_data %>%
+delay_data_pct <- delay_data %>%
   filter(StationCode %in% amtrak_gtfs_feed$stops$stop_id) %>%
   mutate("late_class" = ifelse(sec_diff == 0, "not_late",
                         ifelse(sec_diff > 0 & sec_diff <= 600, "min_late", "late"))) %>%
@@ -9,6 +9,26 @@ delay_data %>%
   group_by(StationCode) %>%
   reframe("pct" = count / sum(count),
           "late_class" = late_class) %>%
+  pivot_wider(names_from = "late_class", values_from = "pct")
+
+delay_data_pct %>%
   #pivot_wider(names_from = late_class, values_from = pct) %>%
+  ggplot(aes(x = pct, fill = late_class)) + 
+  geom_density(alpha = 0.3)
+
+delay_data_pct_train <- delay_data %>%
+  filter(StationCode %in% amtrak_gtfs_feed$stops$stop_id) %>%
+  group_by(train_number) %>%
+  mutate("late_class" = ifelse(sec_diff == 0, "not_late",
+                               ifelse(sec_diff > 0 & sec_diff <= 600, "min_late", "late"))) %>%
+  group_by(train_number, late_class) %>%
+  reframe("count" = n()) %>%
+  group_by(train_number) %>%
+  reframe("pct" = (count / sum(count)),
+          "late_class" = late_class)
+
+delay_data_pct_train$late[is.na(delay_data_pct_train$pct)] <- 0
+
+delay_data_pct_train %>%
   ggplot(aes(x = pct, fill = late_class)) + 
   geom_density(alpha = 0.3)
--- Scripts/trips_to_stop.R
+++ Scripts/trips_to_stop.R
@@ -1,16 +1,8 @@
 library(tidyverse)
 library(sf)
 
-night_stop_one_route <- st_read("Data//Night Routes//night_routes.shp") %>%
+night_stops_ldr <- st_read("Data//Night Routes//night_routes.shp") %>%
   as.data.frame() %>%
-  select(-geometry)
-
-table(night_stop_one_route$on_rt_n)
-
-night_stop_one_route %>%
-  filter(on_rt_n == 1) %>%
+  select(-geometry) %>%
   drop_na(pct_ng_) %>%
-  pull(pct_ng_) %>%
-  table() %>%
-  as.data.frame() %>%
-  pivot_wider(values_from = "Freq", names_from = ".")
+  merge(., delay_data_pct, by.x = "stop_id", by.y = "StationCode")
