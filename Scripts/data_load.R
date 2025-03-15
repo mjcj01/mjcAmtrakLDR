cced960a49e30605779da936eebc83ca9f0044f5
Michael Cattell <mjclego@icloud.com>
2025-03-15 00:05:49 -0400

    edits

--- Scripts/data_load.R
+++ Scripts/data_load.R
@@ -4,5 +4,15 @@ library(sf)
 delay_data <- read_rds("Data//SM_delay_data.rds") %>%
   mutate(StationCode = gsub(" ", "", StationCode))
 
+train_number_list <- delay_data %>%
+  group_by(StationCode, train_number) %>%
+  reframe("count" = n()) %>%
+  filter(count >= 30) %>%
+  pull(train_number) %>%
+  unique()
+
+delay_data <- delay_data %>%
+  filter(train_number %in% train_number_list)
+
 ipcd_stations <- st_read("Data//IPCD Shapefile//Intermodal_Passenger_Connectivity_Database_(IPCD).shp") %>%
   filter(AMTRAKCODE %in% delay_data$StationCode)
